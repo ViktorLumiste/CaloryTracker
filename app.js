a64fe073846df35ef417bcb8857b5d98572876b4
ViktorLumiste <viktor.lumiste@gmail.com>
2022-11-11 10:53:16 +0200

    Added Item to the UI list

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const ItemCtrl = (function(){
 
     const data = {
         items: [
-            {id:0, name:"Steak", calories: 1200},
-            {id:1, name:"Cookie", calories: 400},
-            {id:2, name:"eg", calories: 300}
+            // {id:0, name:"Steak", calories: 1200},
+            // {id:1, name:"Cookie", calories: 400},
+            // {id:2, name:"eg", calories: 300}
         ],
         total: 0
     }
@@ -68,6 +68,17 @@ const UICtrl = (function(){
                 name:document.querySelector(UISelectors.itemNameInput).value,
                 calories:document.querySelector(UISelectors.itemCaloriesInput).value
             }
+        },
+        addListItem: function(item){
+            const li= document.createElement("li")
+            li.className = "collection-item"
+            li.id = `item-${item.id}`
+            li.innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em> <a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>`
+            document.querySelector(UISelectors.itemList).insertAdjacentElement("beforeend", li)
+        },
+        clearInput: function(){
+            document.querySelector(UISelectors.itemNameInput).value = ""
+            document.querySelector(UISelectors.itemCaloriesInput).value = ""
         }
     }
 })();
@@ -81,6 +92,8 @@ const App = (function(ItemCtrl,UICtrl){
         if(input.name !== '' && input.calories !== ''){
             const newItem = ItemCtrl.addItem(input.name, input.calories)
             console.log(newItem)
+            UICtrl.addListItem(newItem)
+            UICtrl.clearInput()
         }
         event.preventDefault()
     }
